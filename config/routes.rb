1abbf6e900734f9751bf566935fc43ab809b9f10
Margaux Pelen <margaux.pelen@gmail.com>
2016-05-12 11:48:19 +0200

    First live pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+
+  resources :ingredients
+  resources :cocktails
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "lemon sky")
+Cocktail.create(name: "ice mind")
+Cocktail.create(name: "mint leaves power")
